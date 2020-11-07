56a6f1e6fefd6fe956bbb6212110753bb532d6ae
John Henry Galino <jhgalino@gmail.com>
2020-11-07 23:10:00 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CS32_ME03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS32_ME03.dir/main.c.o"
+  "CS32_ME03"
+  "CS32_ME03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS32_ME03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CS32_ME03.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CS32_ME03.
+# This may be replaced when dependencies are built.
